124f6f1a988130d72588c27078c9cfe35a50d271
Madiyar <armismadiar20@gmail.com>
2025-11-02 17:34:08 +0500

    Implement Tarjan SCC and add basic JUnit test

--- src/main/java/graph/scc/TarjanSCC.java
+++ src/main/java/graph/scc/TarjanSCC.java
@@ -1,4 +1,56 @@
 package graph.scc;
 
+import java.util.*;
+
 public class TarjanSCC {
+    private final int n;
+    private final List<List<Integer>> adj;
+    private int time = 0;
+    private final int[] disc;
+    private final int[] low;
+    private final boolean[] stackMember;
+    private final Deque<Integer> stack = new ArrayDeque<>();
+
+    public TarjanSCC(int n, List<List<Integer>> adj) {
+        this.n = n;
+        this.adj = adj;
+        disc = new int[n];
+        low = new int[n];
+        stackMember = new boolean[n];
+        Arrays.fill(disc, -1);
+    }
+
+    public List<List<Integer>> getSCCs() {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (disc[i] == -1) dfs(i, res);
+        }
+        return res;
+    }
+
+    private void dfs(int u, List<List<Integer>> res) {
+        disc[u] = low[u] = ++time;
+        stack.push(u);
+        stackMember[u] = true;
+
+        for (int v : adj.get(u)) {
+            if (disc[v] == -1) {
+                dfs(v, res);
+                low[u] = Math.min(low[u], low[v]);
+            } else if (stackMember[v]) {
+                low[u] = Math.min(low[u], disc[v]);
+            }
+        }
+
+        if (low[u] == disc[u]) {
+            List<Integer> scc = new ArrayList<>();
+            int w;
+            do {
+                w = stack.pop();
+                stackMember[w] = false;
+                scc.add(w);
+            } while (w != u);
+            res.add(scc);
+        }
+    }
 }
--- src/test/java/graph/SCCUnitTest.java
+++ src/test/java/graph/SCCUnitTest.java
@@ -1,4 +1,25 @@
 package graph;
 
+import graph.scc.TarjanSCC;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
 public class SCCUnitTest {
+    @Test
+    public void testSimpleSCC() {
+        int n = 4;
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        adj.get(0).add(1);
+        adj.get(1).add(2);
+        adj.get(2).add(0);
+        adj.get(2).add(3);
+
+        TarjanSCC t = new TarjanSCC(n, adj);
+        var sccs = t.getSCCs();
+        assertTrue(sccs.size() > 0);
+        System.out.println("SCC count: " + sccs.size());
+        for (var s : sccs) System.out.println(s);
+    }
 }
